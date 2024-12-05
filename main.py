27bc42f8c5f070253a86e21d98268941a8ec355b
im23b-busere <erik.buser@student.ksh.ch>
2024-12-05 19:39:54 +0100

    Get clipboard text when ctrl+c pressed

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import pyperclip
 import time
 from threading import Thread
+import keyboard
 
 clipboard_history = []
 
 
 def monitor_clipboard():
+    time.sleep(0.1) # fix copied text delay
     current_text = pyperclip.paste()
     clipboard_history.append(current_text)
-    print(f'Kopierter Text: {current_text}')
+    print(f'Kopierte Texte: {clipboard_history}')
 
 
+keyboard.add_hotkey('ctrl+c', monitor_clipboard)
 
 
 
 if __name__ == '__main__':
-    monitor_clipboard()
+    keyboard.wait('esc')
